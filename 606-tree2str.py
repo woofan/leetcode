9192981294b9e7178752e72b6b95614e94c18707
woofan <495464616@qq.com>
2020-12-16 16:41:55 +0800

    12/16

---
+++ 606-tree2str.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def tree2str(self, t: TreeNode) -> str:
+        if t is None:
+            return ''
+        if t.left is None and t.right is None:
+            return str(t.val) + ''
+        if t.right is None:
+            return str(t.val) + '(' + self.tree2str(t.left) +')'
+        return str(t.val) + '(' + self.tree2str(t.left) +')' + '(' +self.tree2str(t.right) + ')
